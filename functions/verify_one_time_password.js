6d84bb2208181c569041367b0803501ffc101285
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-19 21:51:56 -0600

    Added Expo

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -1,17 +1,18 @@
 const admin = require('firebase-admin');
 
 module.exports = function(req, res) {
-  if (!req.body.body || !req.body.code) {
+  if (!req.body.phone || !req.body.code) {
     return res.status(422).send({ error: 'Phone number and code must me provided'});
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
-  const code = parseInt(code);
+  const code = parseInt(req.body.code);
 
   admin.auth().getUser(phone)
     .then(() => {
       const ref = admin.database().ref('users/' + phone);
       ref.on('value', snapshot => {
+        ref.off();
         const user = snapshot.val();
 
         if (user.code !== code || !user.codeValid) {
@@ -19,6 +20,9 @@ module.exports = function(req, res) {
         }
 
         ref.update({ codeValid: false });
+
+        admin.auth().createCustomToken(phone)
+          .then(token => res.send({ token: token }));
       });
     })
     .catch((err) => res.status(422).send({ error: err }));
