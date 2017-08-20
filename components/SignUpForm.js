4e42a54deb321d5347cc68c0b5bbcc9c9c5b98fc
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-20 14:18:15 -0600

    Implemented creating and sending code to user on submit button

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+  const ROOT_URL = 'https://us-central1-one-time-password-3ac48.cloudfunctions.net';
+
+  handleSubmit = () => {
+    axis.post(`${ROOT_URL}/createUser`, {
+      phone: this.state.phone
+    }).then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`,
+          { phone: this.state.phone })
+      })
+  }
+
   render() {
     return (
       <View style={{ marginBottom: 10 }}>
         <FormLabel>Enter Phone Number</FormLabel>
-        <FormInput />
-        <Button title='Submit' />
+        <FormInput
+          value={ this.state.phone }
+          onChangeText={ phone => this.setState({ phone }) }
+        />
+        <Button
+          onPress={ this.handleSubmit }
+          title='Submit'
+        />
       </View>
     );
   }
