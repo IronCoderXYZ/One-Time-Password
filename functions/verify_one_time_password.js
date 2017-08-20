cbe5623ae1ba9e0c6281f7f45dba4c603e5bfa80
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-19 21:36:21 -0600

    Improved verify_one_time_password.js by implementing validation and swapping codeValid property

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -1,3 +1,5 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
   if (!req.body.body || !req.body.code) {
     return res.status(422).send({ error: 'Phone number and code must me provided'});
@@ -6,5 +8,18 @@ module.exports = function(req, res) {
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
   const code = parseInt(code);
 
-  
+  admin.auth().getUser(phone)
+    .then(() => {
+      const ref = admin.database().ref('users/' + phone);
+      ref.on('value', snapshot => {
+        const user = snapshot.val();
+
+        if (user.code !== code || !user.codeValid) {
+          return res.status(422).send({ error: 'Entered code is not valid, please try again' });
+        }
+
+        ref.update({ codeValid: false });
+      });
+    })
+    .catch((err) => res.status(422).send({ error: err }));
 }
