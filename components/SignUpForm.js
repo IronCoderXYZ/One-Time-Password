a5e8c7074637a432ac229835379849bcb006a431
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-20 14:28:38 -0600

    Implemented async/await in handleSubmit function

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -3,17 +3,18 @@ import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
 import axios from 'axios';
 
+const ROOT_URL = 'https://us-central1-one-time-password-3ac48.cloudfunctions.net';
+
 class SignUpForm extends Component {
   state = { phone: '' };
-  const ROOT_URL = 'https://us-central1-one-time-password-3ac48.cloudfunctions.net';
 
-  handleSubmit = () => {
-    axis.post(`${ROOT_URL}/createUser`, {
-      phone: this.state.phone
-    }).then(() => {
-        axios.post(`${ROOT_URL}/requestOneTimePassword`,
-          { phone: this.state.phone })
-      })
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone })
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
